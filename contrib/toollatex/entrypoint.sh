1874b74b775ff2e99ed82397186f5259f65b3e30
Ygenks <kolxozniko@gmail.com>
2020-09-24 00:36:26 +0300

    WIP: first try

--- contrib/toollatex/entrypoint.sh
+++ contrib/toollatex/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-mergerfs -o threads=16,xattr=passthrough,async_read=false,symlinkify=true,posix_acl=true,use_ino,cache.files=off,dropcacheonclose=true,allow_other,category.create=mfs "${CV_CODE}:/_" "${CV_WORKDIR}"
+mergerfs -o threads=0,xattr=passthrough,async_read=false,symlinkify=false,posix_acl=true,use_ino,cache.files=off,dropcacheonclose=true,allow_other,category.create=mfs "${CV_CODE}:/_" "${CV_WORKDIR}"
 cd "${CV_WORKDIR}" || exit 1;
 sudo --user=notroot --group=notroot "$@"
