698b539feb3f0054dd713520872a8a66888bcea6
armoken <alex.armoken@gmail.com>
2021-03-05 00:49:19 +0300

    infra: use Debian Bullseye instead of Sid

--- contrib/toollatex/entrypoint.sh
+++ contrib/toollatex/entrypoint.sh
@@ -2,4 +2,6 @@
 
 mergerfs -o threads=16,xattr=passthrough,async_read=false,symlinkify=true,posix_acl=true,use_ino,cache.files=off,dropcacheonclose=true,allow_other,category.create=mfs "${CV_CODE}:/_" "${CV_WORKDIR}"
 cd "${CV_WORKDIR}" || exit 1;
-sudo --user=notroot --group=notroot "$@"
+sudo --user notroot \
+	 --group $(cat /etc/group | grep ":${HOST_USER_GID}:" | head -1 | cut --delimiter=":" --fields=1) \
+	 "$@"
